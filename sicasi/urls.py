dad12a49c8f0ee487ef140b05785a6404e0d5e32
roger <rogerortiz4@gmail.com>
2023-08-17 21:27:00 -0600

    adding change password logic

--- sicasi/urls.py
+++ sicasi/urls.py
@@ -24,6 +24,8 @@ from courses.views import CourseListView
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("accounts/password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("accounts/password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("admin/", admin.site.urls),
     path("course/", include("courses.urls")),
     path("students/", include("students.urls")),
